22f7ff40d5f06575625ac5d25337155d00eb2a10
pedropark99 <pedropark99@gmail.com>
2023-01-24 18:40:13 -0300

    Add presentation for print screens

--- posts/2023/2023-01-20-storytelling/overview-report.R
+++ posts/2023/2023-01-20-storytelling/overview-report.R
@@ -35,6 +35,8 @@ tema_grafico <- theme(
   legend.title = element_blank()
 )
 
+cores <- c("#e63946", "#1d3557")
+
 
 pl <- df |>
   filter(month(Data) == 11) |>
@@ -48,20 +50,60 @@ pl <- df |>
     size = 1
   ) +
   labs(title = "Evolução diária de usuários ativos") +
-  scale_color_manual(values = c("#e63946", "#1d3557")) +
+  scale_color_manual(values = cores) +
   tema_grafico
 
 ragg::agg_png(
   "posts/2023/2023-01-20-storytelling/usuarios-ativos-diario.png",
-  width = 2000, height = 1100, res = 200
+  width = 2000, height = 1000, res = 200
 )
 print(pl)
 dev.off()
 
 
 
-por_mes |>
+
+
+y <- c(
+  0.05, 0.05, 0.25, 0.25, 0.27, 0.36, 0.42, 0.7, 0.78,
+  0.95, 0.98, 0.98, 0.9, 0.8, 0.78, 0.75, 0.74, 0.7,
+  0.5, 0.4, 0.4, 0.38, 0.35, 0.34
+)
+
+set.seed(40)
+taxa_perdidos <- rnorm(24, 0.1, 0.02)
+taxa_perdidos <- abs(taxa_perdidos)
+
+df <- tibble(
+  Hora = 0:23,
+  `Tickets Atendidos` = y * 2400,
+  `Tickets Perdidos` = y * 2400 * 0.1
+)
+
+
+pl <- df |>
+  pivot_longer(
+    cols = c("Tickets Atendidos", "Tickets Perdidos"),
+    names_to = "Indicador",
+    values_to = "Valor"
+  ) |>
   ggplot() +
-  geom_line(
-    aes(x = as.factor(Mes), y = Usuarios, group = 1)
-  )
+  geom_bar(
+    aes(x = Hora, y = Valor, fill = Indicador),
+    position = "stack", stat = "sum"
+  ) +
+  scale_fill_manual(values = cores) +
+  tema_grafico +
+  labs(title = "Número de atendidos e perdidos por hora") +
+  guides(size = "none")
+
+
+
+
+ragg::agg_png(
+  "posts/2023/2023-01-20-storytelling/tickets-hora.png",
+  width = 2000, height = 1000, res = 200
+)
+print(pl)
+dev.off()
+
