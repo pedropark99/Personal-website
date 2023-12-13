776e399e52c021ff43aa3bcd936bf6f0277c20fc
pedropark99 <pedropark99@gmail.com>
2023-12-13 10:08:16 -0300

    Add content\

--- posts/2023/2023-12-12-zig-strings/hex_to_string.zig
+++ posts/2023/2023-12-12-zig-strings/hex_to_string.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const string_literal = "This is an example of string literal in Zig";
+    const string_literal = "Ⱥ";
     std.debug.print("Bytes that represents the string object: ", .{});
     for (string_literal) |char| {
         std.debug.print("{X} ", .{char});
