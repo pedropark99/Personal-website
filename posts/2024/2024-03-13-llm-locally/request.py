ac769b2df996e2479713e777f476fecb49f7231c
pedropark99 <pedropark99@gmail.com>
2024-03-18 13:15:31 -0300

    Add content

--- posts/2024/2024-03-13-llm-locally/request.py
+++ posts/2024/2024-03-13-llm-locally/request.py
@@ -4,16 +4,14 @@ import re
 
 url = 'http://localhost:11434/api/generate'
 body = '{"model": "codellama:7b", "prompt": "Write me a function that outputs the fibonacci sequence"}'
-
+def parse_response(resp: str) -> dict:
+    resp = resp.content.decode("utf-8")
+    resp = re.sub(r'\}\n\{', '},\n{', resp)
+    resp = f'[{resp}]'
+    return json.loads(resp)
 
 r = requests.post(url, data = body)
-r = str(r.content)
-r = r'[' + r + r']'
-r = re.sub(r'\}\n\{', '},\n{', r)
-print(r)
-print(type(r))
-print(json.loads(r))
-
+r = parse_response(r)
 t = list()
 for token in r:
     t.append(token['response'])
